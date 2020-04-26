8df8d2f88aac07701210e34fb7617afd1d2d398c
Aleks <alexandro361@mail.com>
2020-04-26 22:59:15 +0300

    sql

---
+++ sql.sql
@@ -0,0 +1,3 @@
+create table map ( product_id int foreign key(product.id), category_id int foreign(category.id) );
+
+select prod.name, categ.name from product prod left join map on prod.id = map.product_id left join category categ on map.category_id = categ.id
