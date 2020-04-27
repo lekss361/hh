dad2aea19c47a0b48c99ac0d6c9b22b95c36b4f2
Aleks <alexandro361@mail.com>
2020-04-27 11:16:09 +0300

    input check

--- Calculator Area1/Calculator Area1/MainWindow.xaml.cs
+++ Calculator Area1/Calculator Area1/MainWindow.xaml.cs
@@ -28,22 +28,41 @@ namespace Calculator_Area1
         }
         private void Calculate_Click(object sender, RoutedEventArgs e)
         {
-            double a, b, c, d;
-
+            double a, b, c, d;          
             
-            if (figure.SelectedIndex == 0)
-            {
-                d = double.Parse(Radius.Text);
-                ResultNumber.Text = arearadius(d).ToString();
-            }
-            else if (figure.SelectedIndex == 1)
-            {
-                a = double.Parse(Side1.Text);
-                b = double.Parse(Side2.Text);
-                c = double.Parse(Side3.Text);
-                ResultNumber.Text = areatriangl(a, b, c).ToString();
-                RecTriangle.IsChecked = recTriangle(a, b, c);
-            }
+                if (figure.SelectedIndex == 0)
+                {
+                    if (!Radius.Text.Equals(""))
+                     { 
+                        d = double.Parse(Radius.Text);
+                        ResultNumber.Text = arearadius(d).ToString();
+                    
+                }
+                    else
+                    {
+                     MessageBox.Show("Вы не ввели все необходимые данные!!", "Внимание");
+                     }
+                   
+                }
+                else if (figure.SelectedIndex == 1)
+                {
+                if ((!Side1.Text.Equals("")) || (!Side2.Text.Equals("")) || (!Side3.Text.Equals("")))
+                {
+                    a = double.Parse(Side1.Text);
+                    b = double.Parse(Side2.Text);
+                    c = double.Parse(Side3.Text);
+                    ResultNumber.Text = areatriangl(a, b, c).ToString();
+                    RecTriangle.IsChecked = recTriangle(a, b, c);
+                }
+                else
+                {
+                    MessageBox.Show("Вы не ввели все необходимые данные!!", "Внимание");
+                }
+
+                }
+            
+            
+            
         }
         private void Clear_Click(object sender, RoutedEventArgs e)
         {
@@ -62,8 +81,14 @@ namespace Calculator_Area1
         }
         private double areatriangl(double a, double b, double c)
         {
+            double result;
             double half_per = (a + b + c) / 2;
-            return (Math.Sqrt(half_per * (half_per - a) * (half_per - b) * (half_per - c)));
+            result = Math.Sqrt(half_per * (half_per - a) * (half_per - b) * (half_per - c));
+            if (double.IsNaN(result))
+            {
+                result = 0;
+            }
+            return (result);
         }
         private bool recTriangle(double a, double b, double c)
         {
@@ -92,6 +117,7 @@ namespace Calculator_Area1
         }
         private void textBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
+           
             if (!Char.IsDigit(e.Text, 0))
             {
                 e.Handled = true;
--- sql.sql
+++ sql.sql
@@ -1,3 +1,2 @@
-create table map ( product_id int foreign key(product.id), category_id int foreign(category.id) );
-
+create table map ( product_id int foreign key(product.id), category_id int foreign(category.id)
 select prod.name, categ.name from product prod left join map on prod.id = map.product_id left join category categ on map.category_id = categ.id
